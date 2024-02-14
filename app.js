117c41c3b07bd7da2fb5c33dcf4ddddbab6dcf9f
ttabithar <125708289+ttabithar@users.noreply.github.com>
2024-02-14 11:38:31 -0600

    Update app.js
    added the keyup event listener and changeColor function

--- app.js
+++ app.js
@@ -14,7 +14,37 @@ const boardContainer = document.querySelector(".board");
 document.addEventListener('swiped-up', function(e) {
     console.log(e.target); // the element that was swiped
 });
-    
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        default:
+            return
+    }
+    display()
+    assignRandom()
+    display()
+})
+
+function changeColor(row, col) {
+    let value = board[row][col];
+    return 'hsla(220, ${(100/12)*(Math.log2(value))}%, ${100-Math.log2(value)*12}%,${100-Math.log2(value)/12}%)';
+}
+    assignRandom()
+    assignRandom()
+    display()
+
 function display() {
     let elem = 0;
     for (let row = 0; row < 4; row++) {
